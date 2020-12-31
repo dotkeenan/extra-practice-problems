bb4b7ec16a56afbdf4c99030dfd8cd7325de5b40
Keenan Ng <keenanng.dev@gmail.com>
2020-12-30 19:17:57 -0800

    react button toggle

---
+++ coderbyte/react-button-toggle.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Toggle = () => {
+  const [status, toggleStatus] = useState(true);
+  return (
+    <button onClick={() => toggleStatus(!status)}>{status ? 'ON' : 'OFF'}</button>
+  );
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
+
+/*https://coderbyte.com/editor/frontend:React%20Button%20Toggle
+React Button Toggle
+We provided some simple React template code. Your goal is to modify the component
+so that you can properly toggle the button to switch between an ON state and an
+OFF state. When the button is on and it is clicked, it turns off and the text
+within it changes from ON to OFF and vice versa. Make use of component state for
+this challenge.
+
+You are free to add classes and styles, but make sure you leave the element
+ID's as they are. Submit your code once it is complete and our system will
+validate your output.
+*/
