9937b905fadfd1d327ff624cb822a5444e7bf32e
Keenan Ng <keenanng.dev@gmail.com>
2021-03-28 22:41:48 -0700

    added alternate way to avoid converting arguments object into a real array

--- edabit/concatenateVariableArrays.js
+++ edabit/concatenateVariableArrays.js
@@ -3,6 +3,11 @@ function concat() {
   const res = [];
   for (let i = 0; i < args.length; i++) {
     args[i].forEach(x => res.push(x))
+    // alternate way to iterate over arguments without having to turn it
+    // into a real array:
+    // for(let j = 0; j < arguments[i].length; j++) {
+    //   res.push(arguments[i][j])
+    // }
   }
   return res;
 }
