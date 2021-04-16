1c46da254f68d76d8fc0ca8bfd729cebe817c08a
Keenan Ng <keenanng.dev@gmail.com>
2021-04-15 19:00:18 -0700

    edabit - convert key, values in an object to array

---
+++ edabit/keysAndValuesToArray.test.js
@@ -0,0 +1,15 @@
+const objectToArray = require('./keysAndValuesToArray')
+describe('Convert Key, Values in an Object to Array', () => {
+  test('1', () => {
+    expect(objectToArray({
+      D: 1,
+      B: 2,
+      C: 3
+    })).toEqual([["D", 1], ["B", 2], ["C", 3]])
+  })
+  test(objectToArray({
+    likes: 2,
+    dislikes: 3,
+    followers: 10
+  })).toEqual([["likes", 2], ["dislikes", 3], ["followers", 10]])
+})
