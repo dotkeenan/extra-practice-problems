abc12dcb2f69329b03c9a107a49ff405f949fddf
Keenan Ng <keenanng.dev@gmail.com>
2021-04-15 21:09:46 -0700

    completed exercise and fixed indentation on test

--- edabit/keysAndValuesToArray.test.js
+++ edabit/keysAndValuesToArray.test.js
@@ -9,9 +9,9 @@ describe('Convert Key, Values in an Object to Array', () => {
   })
   test('2', () => {
     expect(objectToArray({
-    likes: 2,
-    dislikes: 3,
-    followers: 10
-  })).toEqual([["likes", 2], ["dislikes", 3], ["followers", 10]])
+      likes: 2,
+      dislikes: 3,
+      followers: 10
+    })).toEqual([["likes", 2], ["dislikes", 3], ["followers", 10]])
   })
 })
