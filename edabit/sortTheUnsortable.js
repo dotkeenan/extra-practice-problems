b10ec5446af7c71151b03805917c83a541ac22a1
Keenan Ng <keenanng.dev@gmail.com>
2021-04-19 23:01:07 -0700

    top solution

--- edabit/sortTheUnsortable.js
+++ edabit/sortTheUnsortable.js
@@ -27,7 +27,7 @@ const sortIt = (arr) => {
 
 module.exports = sortIt;
 
-/*
+/* pseudocode
 Iterate over array.
 add each number to a new array called 'res'
 If iteration is an array, access it's inner value at 0 index, and add that
@@ -37,6 +37,28 @@ sort res
 iterate over arrNums
 splice the occurace of the current arrNums iteration, in 'res', and replace it with
 the same value, but in an array.
+*/
 
+/* top solution
+this is some weird ass approach that I'm not gonna try to delve into.
 
+function spiralNumbers(n) {
+    var matrix = [...Array(n)].map(_ => []),
+        x = 0,
+        y = 0,
+        dir = 2,
+        size = n,
+        c = 0;
+    for (var i = 1; i <= n * n; i++) {
+        matrix[y][x] = i;
+        if (++c == size) {
+            dir = (dir + 1) % 4;
+            size -= dir % 2;
+            c = 0;
+        }
+        if (dir % 2 == 0) x += dir > 1 ? 1 : -1;
+        else y += dir > 1 ? 1 : -1;
+    }
+    return matrix;
+}
 */
