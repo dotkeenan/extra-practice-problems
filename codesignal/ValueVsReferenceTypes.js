08e84d2a7029527704c51ae876dd2756b5e889a6
Keenan Ng <keenanng.dev@gmail.com>
2021-05-08 23:33:31 -0700

    complete

---
+++ codesignal/ValueVsReferenceTypes.js
@@ -0,0 +1,8 @@
+const checkEquals = (arr1, arr2) => {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] != arr2[i]) return false
+    }
+    return true;
+}
+
+module.exports = checkEquals;
