ec31f8b7fedcf67be9e8a861db059c4fe2d0b083
Keenan Ng <keenanng.dev@gmail.com>
2021-05-08 23:34:11 -0700

    added alternative, faster solution

--- codesignal/ValueVsReferenceTypes.js
+++ codesignal/ValueVsReferenceTypes.js
@@ -1,8 +1,9 @@
 const checkEquals = (arr1, arr2) => {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] != arr2[i]) return false
-    }
-    return true;
+    // for (let i = 0; i < arr1.length; i++) {
+    //   if (arr1[i] != arr2[i]) return false
+    // }
+    // return true;
+  return arr1.toString() === arr2.toString();
 }
 
 module.exports = checkEquals;
