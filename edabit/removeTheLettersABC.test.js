4cd91704bf5fe988ec387127b4505adfa0048181
Keenan Ng <keenanng.dev@gmail.com>
2021-05-31 17:46:06 -0700

    edabit - remove the letters ABC - test written

---
+++ edabit/removeTheLettersABC.test.js
@@ -0,0 +1,18 @@
+const removeABC = require('./tileTeamworkTactics')
+describe('Remove the Letters ABC', () => {
+  test('1', () => {
+    expect(removeABC("This might be a bit hard")).toBe("This might e  it hrd")
+    })
+  test('2', () => {
+    expect(removeABC("This is awesome")).toBe("This is wesome")
+    })
+  test('3', () => {
+    expect(removeABC("hello world!")).toBe(null)
+    })
+  test('4', () => {
+    expect(removeABC("coding is fun!")).toBe("oding is fun!")
+    })
+  test('5', () => {
+    expect(removeABC("")).toBe(null)
+    })
+})
