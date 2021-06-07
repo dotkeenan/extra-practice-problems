0b6b47fffa4432b58fbcdd5f29c0042369d5cc7e
Keenan Ng <keenanng.dev@gmail.com>
2021-06-06 21:40:08 -0700

    one-liner

--- edabit/howHeavyIsIt.js
+++ edabit/howHeavyIsIt.js
@@ -1,10 +1,9 @@
 const heavy = (r, h) => {
-  // volume = Math.pi * Math.pow(r, 2) * h
-  let res = Math.PI * Math.pow(r, 2) * h
-  //convert cm^3 to dm^3
-  let resAsDm = res/1000
-  // round and format the number
-  return Number(resAsDm.toFixed(2))
+  // let res = (Math.PI * Math.pow(r, 2) * h) / 1000
+  // return Number(res.toFixed(2))
+
+  // one-liner
+  return Number(((Math.PI * Math.pow(r, 2) * h) / 1000).toFixed(2))
 }
 
 module.exports = heavy;
