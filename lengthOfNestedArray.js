321d2c3c1e20b078bff749eba181ed34b727527d
Keenan Ng <keenanng.dev@gmail.com>
2021-06-11 18:07:20 -0700

    added far better solutions

--- lengthOfNestedArray.js
+++ lengthOfNestedArray.js
@@ -4,14 +4,30 @@ const getLength = (arr) => {
     return arrToCheck.some(x => Array.isArray(x) === true) ? true : false;
   }
   while (checkForArr(arr)) {
-    arr.forEach(x => {
+    // solution 1
+    arr = [].concat(...arr)
+
+    //solution 2
+    // arr = arr.reduce((acc, val) => acc.concat(val), [])
+
+    // solution 3 this is only supported in a few browsers, but is the same thing as above.
+    // arr = arr.flat()
+
+    // My original solution:
+    /* arr.forEach(x => {
       if (Array.isArray(x)) {
         arr.splice(arr.indexOf(x), 1)
         arr = arr.concat(x)
         // console.log(arr) // to test to see the flattening of the nested arrays
       }
-    })
+    }) */
   }
   return arr.length
 }
 module.exports = getLength;
+
+/*
+arr = [ 2, [ 3, [ 4, [Array] ] ], [ 9 ] ]
+arr = [].concat(...[ 2, [ 3, [ 4, [Array] ] ], [ 9 ] ]) //
+
+*/
