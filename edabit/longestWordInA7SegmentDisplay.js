a987cc8ef2f85325facdb59483713fd6c047d4c5
Keenan Ng <keenanng.dev@gmail.com>
2021-06-12 19:04:44 -0700

    completed exercise

--- edabit/longestWordInA7SegmentDisplay.js
+++ edabit/longestWordInA7SegmentDisplay.js
@@ -1,5 +1,18 @@
 const longest7SegmentWord = (arr) => {
-
+  const regex = /[kmvwx]/g
+  const filtered = arr.filter(x => !x.match(regex))
+  if(filtered.length === 0) return '';
+  return filtered.reduce((a,b) => a.length < b.length ? b : a)
+  // return filtered.sort((a,b) => b.length - a.length)[0]
 }
 
 module.exports = longest7SegmentWord;
+
+/*
+create a regex for the letters k,m,v,w,x
+create a new array, using filter, that stores all words that DONT contain the regex letters
+return '' if there are no elements in filtered array.
+
+use a reduce() method to find the first appearing longest word.
+return that result.
+*/
