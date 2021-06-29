976a76cdebe29fbd78d07c107c4c5fbd553e1586
Keenan Ng <keenanng.dev@gmail.com>
2021-06-28 20:40:03 -0700

    fixed non-recursion method.  added description

--- codingJS/recursion1-fibonacci.js
+++ codingJS/recursion1-fibonacci.js
@@ -1,9 +1,21 @@
+/*  description
+The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+
+Examples
+
+fibonacci(0) → 0
+fibonacci(1) → 1
+fibonacci(2) → 1
+*/
+
 /*
-first attempt without recursion
 function fibonacci(n){
   const fibNums = [0,1]
-  for (let i = 2; i < n; i++) {
+  for (let i = 0; i < n-1; i++) {
     fibNums.push(fibNums[fibNums.length-1] + fibNums[fibNums.length-2])
   }
-  return n === 0 ? fibNums[n-1]
-} */
+  return n === 0 ? 0
+       : n === 1 ? 1
+       : fibNums[n]
+}
+ */
