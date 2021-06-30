b1cf30080b050126e19cf82b2aaa2e1a293a94e0
Keenan Ng <keenanng.dev@gmail.com>
2021-06-30 13:31:12 -0700

    expanation

--- codingJS/recursion1-triangle.js
+++ codingJS/recursion1-triangle.js
@@ -11,3 +11,10 @@ triangle(2) → 3
 function triangle(rows){
   return rows === 0 ? 0 : rows + triangle(rows-1)
 }
+
+/*
+the number of rows directly correlates to how many blocks are in that last/largest row
+using, the number of rows as also the number of blocks in a row makes it easy to keep adding
+to keep adding to the previous value of rows in each recursive call.
+calling triangle(rows-1) is the value of the row above the current row.
+*/
