33db628425a8fdf96e34378189066d60613ad470
Keenan Ng <keenanng.dev@gmail.com>
2021-06-30 17:00:07 -0700

    codingJS - recursion1-sumDigits1

---
+++ codingJS/recursion1-sumDigits1.js
@@ -0,0 +1,9 @@
+/*
+Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while flooring division (Math.floor(n/10)) by 10 removes the rightmost digit [Math.floor(126 / 10) is 12].
+
+Examples
+
+sumDigits1(126) → 9
+sumDigits1(49) → 13
+sumDigits1(12) → 3
+*/
