ea12419b589afffd2036ce0214598215f9884612
Keenan Ng <keenanng.dev@gmail.com>
2021-06-30 17:04:10 -0700

    explanation

--- codingJS/recursion1-sumDigits1.js
+++ codingJS/recursion1-sumDigits1.js
@@ -11,3 +11,14 @@ function sumDigits1(n){
   if (Math.floor(n/10) === 0) return n
   return n%10 + sumDigits1(Math.floor(n/10))
 }
+/*
+n % 10 returns the 1's place of the number
+Math.floor(n/10) removes the 1's place of the number
+
+To get a sum of the digits, I want to keep taking the 1's place of the number, then
+'delete' that 1's place digit, and add it to the result of the new number's 1's place digit
+
+To end the recursion, if when trying remove the 1's place of the digit returns 0,
+then just return that last digit, since all that is left is a single digit number.
+
+*/
