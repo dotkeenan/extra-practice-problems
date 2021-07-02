6e9ee0f44f54e7b416ce5f8743c2bba1a6d93042
Keenan Ng <keenanng.dev@gmail.com>
2021-07-02 13:43:01 -0700

    explanation

--- codingJS/recursion1-count8.js
+++ codingJS/recursion1-count8.js
@@ -20,3 +20,25 @@ function count8(n){
       return count8(Math.floor(n/10))
   }
 }
+
+/*
+create the ending condition for the recursion:
+if when attempting to cut off the the 1's place digit, but is unable to (because
+it is already a single digit number), return 1 if the number is 8, or 0 if it's 1.
+
+create a conditional to check if the 1's place digit is an 8 or not:
+
+If it is, use another conditional to check whether the 10's place digit is also
+an 8 or not:
+
+If that nested conditional is true, then return 2 + result of the recursively run function
+called with the number with the 1's place digit removed.
+
+if the above nested conditional does not get triggered, the first conditional should
+return 1 + the result of the recursive function called with the number with the 1's place
+removed.
+
+If the first condition doesn't pass (the ones place digit is not an 8)
+then just return the function recursively called with the number with the 1's place removed.
+
+*/
