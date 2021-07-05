b11f39b0a277627b7d2891f35f3cd9466a307a62
Keenan Ng <keenanng.dev@gmail.com>
2021-07-05 12:20:51 -0700

    exercise complete

--- codingJS/recursion1-countX.js
+++ codingJS/recursion1-countX.js
@@ -7,3 +7,11 @@ countX('xxhixx') → 4
 countX('xhixhix') → 3
 countX('hi') → 0
 */
+function countX(str){
+  if(str.length === 0) return 0;
+  if (str[str.length-1] === 'x') {
+    return 1 + countX(str.slice(0, str.length-1))
+  } else {
+    return countX(str.slice(0, str.length-1))
+  }
+}
