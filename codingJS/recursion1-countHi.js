21bcaecfe792a0c2f3df116e0b5a6292321c67f6
Keenan Ng <keenandng@gmail.com>
2021-07-05 19:51:05 -0700

    description

--- codingJS/recursion1-countHi.js
+++ codingJS/recursion1-countHi.js
@@ -1,3 +1,13 @@
+/*
+Return the number of times that the string "hi" appears anywhere in the given string.
+
+Examples
+
+countHi('abc hi ho') → 1
+countHi('ABChi hi') → 2
+countHi('hihi') → 2
+*/
+
 function countHi(str) {
   if (str.length === 0) return 0
   if (str[str.length - 1] === 'i'
