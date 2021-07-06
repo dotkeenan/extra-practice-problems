26efb18e1452ad211c11045ef40e83995124b998
Keenan Ng <keenandng@gmail.com>
2021-07-05 20:01:36 -0700

    description

--- codingJS/recursion1-countHi.js
+++ codingJS/recursion1-countHi.js
@@ -18,3 +18,18 @@ function countHi(str) {
   }
 }
 console.log(countHi('abc hi ho'))
+
+/*
+recrusion ending condition: if str has no more length, return 0;
+
+to count how many times 'hi' appears in the string
+
+create conditional that checks if the last and second to last
+element in str are 'h' and 'i'.
+-If true, then return 1 + the function recursively called with the str with the last
+letter removed.
+
+if the above condition is not met, then return the same recursively called function
+and argument, but do not add '1'.
+
+*/
