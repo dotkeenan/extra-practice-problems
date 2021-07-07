499a6ee63ce0038097974a29cf42edfab969db19
Keenan Ng <keenanng.dev@gmail.com>
2021-07-06 23:37:05 -0700

    explanation

--- codingJS/recursion1-changeXY.js
+++ codingJS/recursion1-changeXY.js
@@ -11,3 +11,14 @@ function changeXY(str){
   if ((/x/g).test(str) === false) return str;
   return changeXY(str.replace('x','y'))
 }
+
+/*
+create recursion ending condition:
+test if 'str' contains an 'x'.  If it does, return 'str'.
+
+replace() replaces the first found occurance of a string, so basically keep
+replacing until there is nothing else to replace (which the first conditional checks for)
+
+return the function called recursively with the result of 'str' where
+'x' is replaced with 'y'.
+*/
