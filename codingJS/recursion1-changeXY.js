97ec8305af30d53edc8d99fadec0ef4ad3eec4bd
Keenan Ng <keenanng.dev@gmail.com>
2021-07-06 22:27:39 -0700

    recursion1 - changeXY

---
+++ codingJS/recursion1-changeXY.js
@@ -0,0 +1,9 @@
+/* description
+Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars have been changed to 'y' chars.
+
+Examples
+
+changeXY('codex') → codey
+changeXY('xxhixx') → yyhiyy
+changeXY('xhixhix') → yhiyhiy
+*/
