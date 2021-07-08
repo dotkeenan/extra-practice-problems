d7cb6c297f91e3f6eb4346eacef2568e7f5d6dd1
Keenan Ng <keenanng.dev@gmail.com>
2021-07-07 17:46:34 -0700

    codingJS - recrusion1 - changePi

---
+++ codingJS/recursion1-changePi.js
@@ -0,0 +1,9 @@
+/* description
+Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+
+Examples
+
+changePi('xpix') → x3.14x
+changePi('pipi') → 3.143.14
+changePi('pip') → 3.14p
+*/
