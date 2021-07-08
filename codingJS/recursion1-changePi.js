8265a1f78bfb4e7ef6d713913950024023cc79fa
Keenan Ng <keenanng.dev@gmail.com>
2021-07-07 17:46:53 -0700

    exercise complete

--- codingJS/recursion1-changePi.js
+++ codingJS/recursion1-changePi.js
@@ -7,3 +7,8 @@ changePi('xpix') → x3.14x
 changePi('pipi') → 3.143.14
 changePi('pip') → 3.14p
 */
+
+function changePi(str){
+  if ((/pi/g).test(str) === false) return str;
+  return changePi(str.replace('pi', '3.14'))
+}
