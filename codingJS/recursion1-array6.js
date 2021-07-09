e421fe32746855b4df81e80d30cd7b88918cb098
Keenan Ng <keenanng.dev@gmail.com>
2021-07-09 14:40:37 -0700

    explanation

--- codingJS/recursion1-array6.js
+++ codingJS/recursion1-array6.js
@@ -13,3 +13,11 @@ function array6(nums, i){
   if (nums[i]===6) return true
   return array6(nums, i+1)
 }
+
+/*
+two conditions that will end the recursion:
+if nums[i] is 'undefined' (if we are at the end of the array) then return false
+if nums[i] is 6, return true
+
+recursion occurs with an index, 'i', that increments by 1 each call.
+*/
