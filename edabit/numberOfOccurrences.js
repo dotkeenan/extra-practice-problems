e509879ad3465925242dbeb56cd544a3440e8b6e
Keenan Ng <keenanng.dev@gmail.com>
2021-07-12 17:35:53 -0700

    pseudocode

--- edabit/numberOfOccurrences.js
+++ edabit/numberOfOccurrences.js
@@ -3,3 +3,17 @@ const numberOfOccurrences = (obj) => {
 }
 
 module.exports = numberOfOccurrences;
+
+/*
+create an empty object named 'res'
+loop over the object with a for...in loop
+
+If res doesn't contain the the current iterations property,
+add the current iteration as a key, and the number 1, as a value.
+
+If res DOES contain the current iteration's property already,
+increment the value of that property in 'res'.
+
+return res.
+
+*/
