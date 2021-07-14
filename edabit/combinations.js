57ef5223b4f85dac56fe815c35b7df7ebbb8733c
Keenan Ng <keenanng.dev@gmail.com>
2021-07-13 19:03:12 -0700

    explanation

--- edabit/combinations.js
+++ edabit/combinations.js
@@ -5,3 +5,15 @@ const combinations = (...args) => {
 }
 
 module.exports = combinations;
+
+/*
+use the rest parameter (es6) for the 'combinations' function
+turn the arguments into an array.  In my case, I also wanted to get rid of zeros,
+so I used filter().
+
+I returned the result of reducing the new argsArr array by multiplying the accumulator
+by the current.
+
+If the array only has 1 number, it smartly just returns that 1 number, instead of trying to
+do something like argsArr[0] * undefined === undefined/NaN
+*/
